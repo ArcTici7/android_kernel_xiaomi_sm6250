45d4001dad75de5ca79cae3567fa2b205fd60acf
ArcWire__xD <108523359+ArcTici7@users.noreply.github.com>
2026-04-19 18:34:10 -0700

    Update u_audio.h

--- drivers/usb/gadget/function/u_audio.h
+++ drivers/usb/gadget/function/u_audio.h
@@ -1,95 +1,48 @@
-/*
- * u_audio.h -- interface to USB gadget "ALSA sound card" utilities
- *
- * Copyright (C) 2016
- * Author: Ruslan Bilovol <ruslan.bilovol@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- */
-
-#ifndef __U_AUDIO_H
-#define __U_AUDIO_H
+#ifndef __U_UAC2_H
+#define __U_UAC2_H
 
 #include <linux/usb/composite.h>
 
-struct uac_params {
-	/* playback */
-	int p_chmask;	/* channel mask */
-	int p_srate;	/* rate in Hz */
-	int p_ssize;	/* sample size */
+/* =========================================================
+ * UAC2 TOPOLOGY IDS (MUST MATCH f_uac2.c)
+ * ========================================================= */
 
-	/* capture */
-	int c_chmask;	/* channel mask */
-	int c_srate;	/* rate in Hz */
-	int c_ssize;	/* sample size */
+#define UAC2_OUT_IT_ID      1
+#define UAC2_IN_IT_ID       2
+#define UAC2_OUT_OT_ID      3
+#define UAC2_IN_OT_ID       4
 
-	int req_number; /* number of preallocated requests */
-};
+#define UAC2_CLK_OUT_ID     5
+#define UAC2_CLK_IN_ID      6
 
-struct g_audio {
-	struct usb_function func;
-	struct usb_gadget *gadget;
+/* =========================================================
+ * AUDIO FORMAT (FIXED FOR WINDOWS COMPATIBILITY)
+ * ========================================================= */
 
-	struct usb_ep *in_ep;
-	struct usb_ep *out_ep;
+#define UAC2_FORMAT_RATE     48000
+#define UAC2_CHANNELS        2
+#define UAC2_SAMPLE_SIZE     2   /* 16-bit PCM */
 
-	/* Max packet size for all in_ep possible speeds */
-	unsigned int in_ep_maxpsize;
-	/* Max packet size for all out_ep possible speeds */
-	unsigned int out_ep_maxpsize;
+/* =========================================================
+ * PACKET BEHAVIOR (CRITICAL CONSISTENCY)
+ * ========================================================= */
 
-	/* The ALSA Sound Card it represents on the USB-Client side */
-	struct snd_uac_chip *uac;
+#define UAC2_INTERVAL        1
+#define UAC2_MAX_PACKET_FS   200
+#define UAC2_MAX_PACKET_HS   1024
 
-	struct uac_params params;
-};
+/* =========================================================
+ * SYNC MODE (MUST MATCH ENDPOINTS)
+ * ========================================================= */
 
-static inline struct g_audio *func_to_g_audio(struct usb_function *f)
-{
-	return container_of(f, struct g_audio, func);
-}
+#define UAC2_SYNC_ASYNC_OUT   1
+#define UAC2_SYNC_ADAPTIVE_IN 1
 
-static inline uint num_channels(uint chanmask)
-{
-	uint num = 0;
+/* =========================================================
+ * BUFFERING (LOW LATENCY SAFE)
+ * ========================================================= */
 
-	while (chanmask) {
-		num += (chanmask & 1);
-		chanmask >>= 1;
-	}
+#define UAC2_MIN_PERIODS      4
+#define UAC2_MAX_PERIODS      8
 
-	return num;
-}
-
-/*
- * g_audio_setup - initialize one virtual ALSA sound card
- * @g_audio: struct with filled params, in_ep_maxpsize, out_ep_maxpsize
- * @pcm_name: the id string for a PCM instance of this sound card
- * @card_name: name of this soundcard
- *
- * This sets up the single virtual ALSA sound card that may be exported by a
- * gadget driver using this framework.
- *
- * Context: may sleep
- *
- * Returns zero on success, or a negative error on failure.
- */
-int g_audio_setup(struct g_audio *g_audio, const char *pcm_name,
-					const char *card_name);
-void g_audio_cleanup(struct g_audio *g_audio);
-
-int u_audio_start_capture(struct g_audio *g_audio);
-void u_audio_stop_capture(struct g_audio *g_audio);
-int u_audio_start_playback(struct g_audio *g_audio);
-void u_audio_stop_playback(struct g_audio *g_audio);
-
-#endif /* __U_AUDIO_H */
+#endif
