f28fdd555181762671d9a525b5c15b438936111e
ArcWire__xD <108523359+ArcTici7@users.noreply.github.com>
2026-04-19 18:40:49 -0700

    Update u_audio.h

--- drivers/usb/gadget/function/u_audio.h
+++ drivers/usb/gadget/function/u_audio.h
@@ -1,10 +1,10 @@
 #ifndef U_UAC2_H
 #define U_UAC2_H
 
-#define UAC2_MAX_CHANNELS 2
-#define UAC2_SAMPLE_SIZE  2
-#define UAC2_SAMPLE_RATE  48000
+#define UAC2_RATE 48000
+#define UAC2_CH   2
+#define UAC2_SS   2
 
-#define UAC2_PERIOD_SIZE  192
+#define UAC2_PKT 192
 
 #endif
